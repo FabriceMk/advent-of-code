15bf4cfe6279f1780e3628afb11f5f9b57ea91d6
FabriceMk <5099424+FabriceMk@users.noreply.github.com>
2024-12-26 18:10:44 +0900

    Day 22 P2 and Day 25 P1

---
+++ 2024-csharp/day22/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Diagnostics;
+
+// Extract argument to know which part to run
+var part = args.FirstOrDefault();
+
+Console.WriteLine("##########");
+Console.WriteLine($"Running part {part}");
+
+var filename = (args.Length > 1 && args[1] == "test") ? "input-test" : "input-full";
+string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename));
+
+var watch = Stopwatch.StartNew();
+
+var res = (part == "1") ? Part1(input) : Part2(input);
+
+watch.Stop();
+
+Console.WriteLine("##########");
+Console.WriteLine($"Execution time: {watch.ElapsedMilliseconds}ms");
+Console.WriteLine("Output:");
+Console.WriteLine(res);
+Console.WriteLine("##########");
+
+long Part1(string[] input)
+{
+    long result = 0;
+    var iterations = 2000;
+
+    foreach (var line in input)
+    {
+        long.TryParse(line, out var secretNumber);
+
+        for (var i = 0; i < iterations; i++)
+        {
+            secretNumber = nextSecret(secretNumber);
+        }
+
+       result += secretNumber;
+    }
+
+    return result;
+}
+
+long nextSecret(long secretNumber)
+{
+    var temp = secretNumber * 64;
+    secretNumber ^= temp;
+    secretNumber %= 16777216;
+    temp = (long) Math.Floor((double) secretNumber/32);
+    secretNumber ^= temp;
+    secretNumber %= 16777216;
+    temp = secretNumber * 2048;
+    secretNumber ^= temp;
+    secretNumber %= 16777216;
+
+    return secretNumber;
+}
+
+
+int Part2(string[] input)
+{
+    var result = 0;
+
+    var iterations = 2000;
+
+    var currentSeller = 0;
+    var sequences = new Dictionary<(int, int, int, int), Dictionary<int,int>>();
+
+    foreach (var line in input)
+    {
+        currentSeller++;
+
+        long.TryParse(line, out var secretNumber);
+
+        var lastChanges = new Queue<int>();
+
+        secretNumber = nextSecret(secretNumber);
+        var previousSellingPrice = (int) secretNumber % 10;
+
+        for (var i = 1; i < iterations; i++)
+        {
+            secretNumber = nextSecret(secretNumber);
+            var currentSellingPrice = (int) secretNumber % 10;
+
+            lastChanges.Enqueue(currentSellingPrice - previousSellingPrice);
+            previousSellingPrice = currentSellingPrice;
+
+            if (lastChanges.Count > 4) {
+                lastChanges.Dequeue();
+            }
+
+            if (lastChanges.Count < 4) {
+                continue;
+            }
+
+            var sequence = (lastChanges.ElementAt(0), lastChanges.ElementAt(1), lastChanges.ElementAt(2), lastChanges.ElementAt(3));
+
+            var foundExistingSequence = sequences.TryGetValue(sequence, out var _);
+
+            if (foundExistingSequence) {
+                var foundPriceForSellerSequence = sequences[sequence].TryGetValue(currentSeller, out var _);
+                if (!foundPriceForSellerSequence) {
+                    sequences[sequence][currentSeller] = currentSellingPrice;
+                }
+            } else {
+                sequences[sequence] = new Dictionary<int, int> {{ currentSeller, currentSellingPrice }};
+            }
+        }
+    }
+
+    foreach (var sequence in sequences)
+    {
+        var maxBananasForSequence = sequence.Value.Values.Sum();
+        if (maxBananasForSequence > result)
+        {
+            result = maxBananasForSequence;
+        }
+    }
+
+    return result;
+}
---
+++ 2024-csharp/day25/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Diagnostics;
+
+// Extract argument to know which part to run
+var part = args.FirstOrDefault();
+
+Console.WriteLine("##########");
+Console.WriteLine($"Running part {part}");
+
+var filename = (args.Length > 1 && args[1] == "test") ? "input-test" : "input-full";
+string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename));
+
+var watch = Stopwatch.StartNew();
+
+var res = (part == "1") ? Part1(input) : Part2(input);
+
+watch.Stop();
+
+Console.WriteLine("##########");
+Console.WriteLine($"Execution time: {watch.ElapsedMilliseconds}ms");
+Console.WriteLine("Output:");
+Console.WriteLine(res);
+Console.WriteLine("##########");
+
+int Part1(string[] input)
+{
+    int result = 0;
+
+    var listKeys = new List<(int,int,int,int,int)>();
+    var listLocks = new List<(int,int,int,int,int)>();
+
+    var parsingKey = false;
+    var currentlyParsing = false;
+    var currentTuple = (0,0,0,0,0);
+
+    foreach (var line in input)
+    {
+        if (line == string.Empty)
+        {
+            currentlyParsing = false;
+
+            if (parsingKey)
+            {
+                // Reduce the height of 1 to take into account the '#####' floor for keys.
+                listKeys.Add((currentTuple.Item1-1, currentTuple.Item2-1, currentTuple.Item3-1, currentTuple.Item4-1, currentTuple.Item5-1));
+            } else {
+                listLocks.Add((currentTuple.Item1, currentTuple.Item2, currentTuple.Item3, currentTuple.Item4, currentTuple.Item5));
+            }
+
+            currentTuple = (0,0,0,0,0);
+
+            continue;
+        }
+
+        if (!currentlyParsing)
+        {
+            currentlyParsing = true;
+            parsingKey = line == ".....";
+            continue;
+        }
+
+        currentTuple.Item1 += line[0] == '#' ? 1 : 0;
+        currentTuple.Item2 += line[1] == '#' ? 1 : 0;
+        currentTuple.Item3 += line[2] == '#' ? 1 : 0;
+        currentTuple.Item4 += line[3] == '#' ? 1 : 0;
+        currentTuple.Item5 += line[4] == '#' ? 1 : 0;
+    }
+
+    Console.WriteLine("locks");
+    foreach(var month in listLocks)
+    {
+        Console.WriteLine(month);
+    }
+
+    Console.WriteLine("keys");
+    foreach(var month in listKeys)
+    {
+        Console.WriteLine(month);
+    }
+
+    foreach (var key in listKeys)
+    {
+        foreach (var currentLock in listLocks)
+        {
+            if (currentLock.Item1 + key.Item1 <= 5 &&
+            currentLock.Item2 + key.Item2 <= 5 &&
+            currentLock.Item3 + key.Item3 <= 5 &&
+            currentLock.Item4 + key.Item4 <= 5 &&
+            currentLock.Item5 + key.Item5 <= 5) {
+                result += 1;
+            }
+        }
+    }
+
+    return result;
+}
+
+int Part2(string[] input)
+{
+    var result = 0;
+
+    return result;
+}
